c17028212285961e06fd6bd4faf8f0da5d9f2f92
eemr3 <eemr3@yahoo.com.br>
2022-10-19 11:53:15 -0300

    feat(auth): implement login authGuardian

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
+// import { JwtStrategy } from './strategies/jwt.strategy';
+// import { LocalStrategy } from './strategies/local.strategy';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
